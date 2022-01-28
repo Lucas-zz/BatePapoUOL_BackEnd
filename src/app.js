b1c92fb766d485bea8dfdd03c1c16868df581ae4
Lucas <lucasazzollinivieira@gmail.com>
2022-01-28 00:52:23 -0300

    /participants route done, still needs testing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import { MongoClient, ObjectId } from 'mongodb';
+import dayjs from 'dayjs';
 import joi from 'joi';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
@@ -11,6 +12,42 @@ const port = 5000;
 
 app.use(cors());
 app.use(json());
+
+const participantScheme = joi.object({
+    name: joi.string().required(),
+});
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+    db = mongoClient.db("batePapoUOL_backEnd");
+});
+
+app.post('/participants', async (request, response) => {
+    try {
+        const { name } = request.body;
+
+        const isRegistered = await db.collection('participants').findOne({ name: name });
+        if (isRegistered) {
+            return response.status(409).send("Nome já existe. Por favor, escolha outro.")
+        }
+
+        const validation = participantScheme.validate(request.body, { abortEarly: true });
+
+        if (validation.error) {
+            return response.status(422);
+        }
+
+        const time = dayjs().locale('pt-br').format('HH:MM:SS')
+
+        await db.collection('participants').insertOne({ "name": name, "lastStatus": Date.now() });
+        await db.collection('messages').insertOne({ "from": name, "to": "Todos", "text": "entra na sala", "type": "status", "time": time });
+        response.sendStatus(201);
+    } catch (error) {
+        console.error(error);
+        response.sendStatus(500);
+    }
+});
 
 
 app.listen(port, () => {
